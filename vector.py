42af05983c13c386665ac99f5910aee0095214c9
Christian Acuna <christiandeeplearning0828@gmail.com>
2020-07-19 16:44:37 -0700

    Add +,-,* methods

--- vector.py
+++ vector.py
@@ -14,6 +14,20 @@ class Vector(object):
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
+    def __repr__(self):
+      return 'Vector: {}'.format(self.coordinates)
+
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
+    def __add__(self, v):
+      new_coordinates = [x + y for x, y in zip(self.coordinates, v.coordinates)]
+      return Vector(new_coordinates)
+
+    def __sub__(self, v):
+      new_coordinates = [x - y for x, y in zip(self.coordinates, v.coordinates)]
+      return Vector(new_coordinates)
+
+    def __mul__(self, scalar):
+      new_coordinates = [scalar * x for x in self.coordinates]
+      return Vector(new_coordinates)
